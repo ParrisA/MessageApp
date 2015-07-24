8dba6877a1c2ff63ce05c07a744faa44fe70dcd8
Parris Alleyne <parris223344@gmail.com>
2015-07-23 23:07:29 -0400

    Stopped sending email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :myself_only, only: [:edit, :update, :destroy]
+  """before_action :myself_only, only: [:edit, :update, :destroy]"""
   # GET /users
   # GET /users.json
   def index
